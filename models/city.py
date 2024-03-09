5b1b615d2c0e4732259c62b4a0c39dba882a9d5e
mosescode1 <emosescode@gmail.com>
2024-03-09 19:10:10 +0100

    update filestorage to manage serlization and deserilization for other classes

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for City"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for City"""
+
+    name = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ File Storage Module """
+
 import json
 # from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -13,7 +19,12 @@ class FileStorage:
     __objects = dict()
     definedclass = {
         'BaseModel': BaseModel,
-        'User': User
+        'User': User,
+        'State': State,
+        'Place': Place,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review
     }
 
     def all(self):
