59e0dfb844dcb7ce7088296b1d3b84670d28f4c8
mosescode1 <emosescode@gmail.com>
2024-03-11 16:51:28 +0100

    fixed

--- models/place.py
+++ models/place.py
@@ -7,14 +7,14 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """Class for Place"""
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = ""
+    city_id = None
+    user_id = None
+    name = None
+    description = None
+    number_rooms = None
+    number_bathrooms = None
+    max_guest = None
+    price_by_night = None
+    latitude = None
+    longitude = None
+    amenity_ids = None
